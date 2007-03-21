1033fa695536517fc9688e49b3e856e77ab631bc
Ricardo SIGNES <rjbs@cpan.org>
2007-03-21 02:15:37 +0000

    Import of RJBS/Email-ARF-0.000 from CPAN.
    gitpan-cpan-distribution: Email-ARF
    gitpan-cpan-version:      0.000
    gitpan-cpan-path:         RJBS/Email-ARF-0.000.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

---
+++ lib/Email/ARF.pm
@@ -0,0 +1,20 @@
+
+package Email::ARF;
+
+=head1 NAME
+
+Email::ARF - abuse report format (placeholder module)
+
+=head1 VERSION
+
+version 0.000
+
+  $Id: /my/pep/Email-ARF/trunk/lib/Email/ARF.pm 31005 2007-03-21T02:10:59.930917Z rjbs  $
+
+=cut
+
+our $VERSION = '0.000';
+
+warn "This module does nothing." unless $ENV{HARNESS_ACTIVE};
+
+1;
